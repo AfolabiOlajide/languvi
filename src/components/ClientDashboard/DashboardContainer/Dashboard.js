2c9e7e835cec58c830061c525babce7b17c0b436
Afolabi Olajide Samuel <samolajide20@gmail.com>
2022-12-26 06:45:14 +0100

    dashboard section and responsive menu

--- src/components/ClientDashboard/DashboardContainer/Dashboard.js
+++ src/components/ClientDashboard/DashboardContainer/Dashboard.js
@@ -1,8 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react';
+import WelcomeImg from "../../../assets/welImg.png"
 
 const Dashboard = () => {
+    const [welcomeImgAvailable, setWelcomeImgAvailable] = useState(true);
+
+    const toggleWelcomeImgHandler = () =>{
+        setWelcomeImgAvailable(!welcomeImgAvailable);
+    }
+
     return (
-        <div>Dashboard</div>
+        <div className='px-6 py-3'>
+            <div className="welcome">
+                <img src={WelcomeImg} className={`w-[50%] ${welcomeImgAvailable ? "block" : "hidden"}`} alt="Welcome" onClick={toggleWelcomeImgHandler} />
+            </div>
+        </div>
     )
 }
 
--- src/components/DashboardLayout.js
+++ src/components/DashboardLayout.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
+import moment from 'moment/moment';
 import { Link, NavLink, Outlet } from 'react-router-dom';
 import { FiLogOut } from "react-icons/fi";
-import MenuDesign from "../assets/menu-design-img.png"
+import { HiOutlineMenuAlt1 } from "react-icons/hi";
+import { MdOutlineClose } from "react-icons/md"
+import MenuDesign from "../assets/menu-design-img.png";
 
 const DashboardLayout = () => {
+    const [menuToggled, setMenuToggled] = useState(false);
+    const date = moment().format("Do MMMM YYYY");
+
+    const menuToggledHandler = () => {
+        setMenuToggled(!menuToggled);
+    }
+
     return (
-        <div className='grid grid-cols-[20%_80%] gap-x-4'>
-            <div className="dashboard-menu py-6 px-4 bg-white min-h-screen min-w-[20%] border-r-2 border-slate-100 ">
+        <div className='lg:grid lg:grid-cols-[20%_80%]'>
+            <div className={`absolute top-0 ${menuToggled ? "left-0" : "-left-[110%]"} duration-300 ease-in transition-all z-50 lg:relative dashboard-menu py-6 px-4 bg-white min-h-screen min-w-[20%] border-r-2 border-slate-100 `}>
                 <div className="menu-header flex flex-row gap-4">
                     <div className="img">
                         <div className="w-[4rem] h-[4rem] rounded-md bg-primary"></div>
@@ -17,22 +27,22 @@ const DashboardLayout = () => {
                     </div>
                 </div>
                 <div className="menu-link flex flex-col gap-y-2">
-                    <Link to="/client/dashboard">
+                    <Link to="/client/dashboard" onClick={menuToggledHandler}>
                         <div className="link-item mt-6 font-bold text-primary rounded-md text-center border border-primaryLight p-3">Dashboard</div>
                     </Link>
-                    <NavLink to="/client/dashboard/orders" className={({ isActive }) => isActive ? "menu-hover bg-primaryLight text-primaryDark px-4 py-3 rounded-md" : "menu-hover text-slate-400 px-4 py-3"}>
+                    <NavLink to="/client/dashboard/orders" className={({ isActive }) => isActive ? "menu-hover bg-primaryLight text-primaryDark px-4 py-3 rounded-md" : "menu-hover text-slate-400 px-4 py-3"} onClick={menuToggledHandler}>
                         <div className="link-item flex gap-x-4 items-center">My Orders</div>
                     </NavLink>
-                    <NavLink to="/client/dashboard/offers" className={({ isActive }) => isActive ? "menu-hover bg-primaryLight text-primaryDark px-4 py-3 rounded-md" : "menu-hover text-slate-400 px-4 py-3"}>
+                    <NavLink to="/client/dashboard/offers" className={({ isActive }) => isActive ? "menu-hover bg-primaryLight text-primaryDark px-4 py-3 rounded-md" : "menu-hover text-slate-400 px-4 py-3"} onClick={menuToggledHandler}>
                         <div className="link-item flex gap-x-4 items-center">Offers</div>
                     </NavLink>
-                    <NavLink to="/client/dashboard/revisions" className={({ isActive }) => isActive ? "menu-hover bg-primaryLight text-primaryDark px-4 py-3 rounded-md" : "menu-hover text-slate-400 px-4 py-3"}>
+                    <NavLink to="/client/dashboard/revisions" className={({ isActive }) => isActive ? "menu-hover bg-primaryLight text-primaryDark px-4 py-3 rounded-md" : "menu-hover text-slate-400 px-4 py-3"} onClick={menuToggledHandler}>
                         <div className="link-item flex gap-x-4 items-center">Revisions</div>
                     </NavLink>
-                    <NavLink to="/client/dashboard/contact-us" className={({ isActive }) => isActive ? "menu-hover bg-primaryLight text-primaryDark px-4 py-3 rounded-md" : "menu-hover text-slate-400 px-4 py-3"}>
+                    <NavLink to="/client/dashboard/contact-us" className={({ isActive }) => isActive ? "menu-hover bg-primaryLight text-primaryDark px-4 py-3 rounded-md" : "menu-hover text-slate-400 px-4 py-3"} onClick={menuToggledHandler}>
                         <div className="link-item flex gap-x-4 items-center">Contact Us</div>
                     </NavLink>
-                    <NavLink to="/client/dashboard/settings" className={({ isActive }) => isActive ? "menu-hover bg-primaryLight text-primaryDark px-4 py-3 rounded-md" : "menu-hover text-slate-400 px-4 py-3"}>
+                    <NavLink to="/client/dashboard/settings" className={({ isActive }) => isActive ? "menu-hover bg-primaryLight text-primaryDark px-4 py-3 rounded-md" : "menu-hover text-slate-400 px-4 py-3"} onClick={menuToggledHandler}>
                         <div className="link-item flex gap-x-4 items-center">Settings</div>
                     </NavLink>
                 </div>
@@ -46,6 +56,20 @@ const DashboardLayout = () => {
                 </div>                
             </div>
             <div className="view">
+                <div className="dashboard-header p-6 bg-zinc-300 flex justify-between">
+                    <div className="left flex flex-col gap-y-4">
+                        <div className="logo">
+                            <h1 className='text-primary font-bold text-4xl'>Languvi</h1>
+                        </div>
+                        <div className="tag bg-green-500 rounded-sm text-white text-[.7rem] text-center p-1">Client Admin</div>
+                    </div>
+                    <div className="right flex items-center gap-x-6">
+                        <div className="date text-xl font-bold">{date}</div>
+                        <div className="toggle-menu">
+                            {menuToggled ? <MdOutlineClose className='block cursor-pointer lg:hidden' onClick={menuToggledHandler} size={24} /> : <HiOutlineMenuAlt1 className='block cursor-pointer lg:hidden' onClick={menuToggledHandler} size={24} />}
+                        </div>
+                    </div>
+                </div>
                 <Outlet />
             </div>
         </div>
